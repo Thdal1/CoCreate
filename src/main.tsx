400dc81037cfd9b97eceab9fcb532973f4863c1e
THDalen <56918450+THDalen@users.noreply.github.com>
2026-01-07 21:45:18 +0100

    testing

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Data from './data/test.json'
 import App from './App.tsx'
-import Banner from './components/event-banner/event-banner.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <App /> */}
+    <App />
 
-    <Banner data={Data} />
   </StrictMode>,
 )
